d1d60f7fd39c890b569cc283d0da970920e57a52
Racheron-create <77326392+Racheron-create@users.noreply.github.com>
2021-11-08 12:27:39 +0000

    Adding Outdoor Ammenities

---
+++ ParkType.java
@@ -0,0 +1,18 @@
+/**
+ * Enum with constants representing the different parking facilities
+ * 
+ * @version 1.0 
+ *
+ * @author Rachel Parker
+ *
+ */
+
+public enum ParkType {
+	free("free on-site parking"),
+	onRoad("on road parking"),
+	paid("paid car-park");
+	
+	private final String ParkType;
+	
+	public String getString() {return ParkType;}
+}
