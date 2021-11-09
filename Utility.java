bd164bfd8b0091d36af649212ce2cd8aa7fe557b
Racheron-create <77326392+Racheron-create@users.noreply.github.com>
2021-11-09 13:10:26 +0000

    Create Utility Amenity

---
+++ Utility.java
@@ -0,0 +1,77 @@
+/**
+ * Class designed to create Utility Amenity
+ * 
+ * @version 1.0 
+ *
+ * @author aca17rhp
+ *
+ */
+
+public class Utility {
+	final boolean centralHeating;
+	final boolean washingMachine;
+	final boolean dryingMachine;
+	final boolean fireExtinguisher;
+	final boolean smokeAlarm;
+	final boolean firstAid;
+	
+	// utility space creator
+	public Utility(boolean centralHeating, boolean washingMachine, boolean dryingMachine, boolean fireExtinguisher, boolean smokeAlarm, 
+			boolean firstAid) {
+		this.centralHeating = centralHeating;
+		this.washingMachine = washingMachine;
+		this.dryingMachine = dryingMachine;
+		this.fireExtinguisher = fireExtinguisher;
+		this.smokeAlarm = smokeAlarm;
+		this.firstAid = firstAid;
+	}
+		
+	public boolean getCentralHeating() {
+		return this.centralHeating;
+	}
+	
+	public boolean getWashingMachine() {
+		return this.washingMachine;
+	}
+	
+	public boolean getDryingMachine() {
+		return this.dryingMachine;
+	}
+	
+	public boolean getFireExtinguisher() {
+		return this.fireExtinguisher;
+	}
+	
+	public boolean getSmokeAlarm() {
+		return this.smokeAlarm;
+	}
+	
+	public boolean getFirstAid() {
+		return this.firstAid;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("Central Heating: ");
+		sb.append(getCentralHeating());
+		sb.append("\n");
+		sb.append("Washing Machine: ");
+		sb.append(getWashingMachine());
+		sb.append("\n");
+		sb.append("Drying Machine: ");
+		sb.append(getDryingMachine());
+		sb.append("\n");
+		sb.append("Fire Extinguisher: ");
+		sb.append(getFireExtinguisher());
+		sb.append("\n");
+		sb.append("Smoke Alarm: ");
+		sb.append(getSmokeAlarm());
+		sb.append("\n");
+		sb.append("First Aid: ");
+		sb.append(getFirstAid());
+	
+		return sb.toString();
+	}
+		
+}
