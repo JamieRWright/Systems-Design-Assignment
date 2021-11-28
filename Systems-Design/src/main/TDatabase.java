5e803b7728106f01290584599620c1ab6dfa7d6f
Jamie Wright <45731586+JamieRWright@users.noreply.github.com>
2021-11-28 13:42:52 +0000

    Added SHA-1 Hashing

--- Systems-Design/src/main/TDatabase.java
+++ Systems-Design/src/main/TDatabase.java
@@ -7,6 +7,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.*;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 
 //Static class created to be used throughout the project
 //Methods are called like this: TDatabase.SearchFullTable("Guest")
@@ -20,6 +24,8 @@ public final class TDatabase {
 	public static Map<Integer, Booking> Bookings = null;
 	public static Map<Integer, Guest> Guests = null;
 	public static Map<Integer, Host> Hosts = null;
+	public static Map<Integer, Booking> Bookings = null;
+
 	private static Connection con;
 	private static String _SQLGuest = "SELECT * FROM Guest;";
 	private static String _SQLHost = "SELECT * FROM Host;";
@@ -38,6 +44,32 @@ public final class TDatabase {
 		
 		return isSuccess;
 	}
+	
+	public static String encryptThisString(String input)
+	{
+		try {
+			// getInstance() method is called with algorithm SHA-1
+			MessageDigest md = MessageDigest.getInstance("SHA-1");
+
+			// digest() method is called
+			// to calculate message digest of the input string
+			// returned as array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+
+			// Add preceding 0s to make it 32 bit
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+
+			// return the HashText
+			return hashtext;
+		}
 	
 		private static Map<Integer, Host> LoadHosts() {
 		 Map<Integer, Host> output = new HashMap<Integer, Host>();
