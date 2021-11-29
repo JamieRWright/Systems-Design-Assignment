6ef7eb79a4b3dff0ffb233bfcddb29770cea18d4
Racheron-create <77326392+Racheron-create@users.noreply.github.com>
2021-11-29 16:29:59 +0000

    adding Setters

--- Systems-Design/src/main/Utility.java
+++ Systems-Design/src/main/Utility.java
@@ -42,6 +42,85 @@ public class Utility {
 		return this.firstAid;
 	}
 	
+	public boolean setCentral(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "CentralHeating", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "CentralHeating", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	public boolean setWashing(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "WashingMachine", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "WashingMachine", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	public boolean setDrying(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "DryingMachine", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "DryingMachine", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	public boolean setFire(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "FireExtinguisher", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "FireExtinguisher", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	public boolean setSmoke(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "SmokeAlarm", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "SmokeAlarm", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	public boolean setFirst(boolean value, Integer PropertyID)
+	{
+		boolean output=false;
+		if (value==true)
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "FirstAid", PropertyID.toString(), 1);
+		}
+		else
+		{
+			output=TDatabase.UpdateFacilityValue("Utility_Facility", "FirstAid", PropertyID.toString(), 0);
+		}
+		return output;
+	}
+	
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
 		
